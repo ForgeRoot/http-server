e3659eb2479fc1ce76008b4369d406600937f282
ForgeRoot <dylananne.v.eck@gmail.com>
2026-07-02 19:24:16 +0200

    refactor - stateful handler to multiple files

--- main.go
+++ main.go
@@ -1,47 +1,29 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "io"
-  "sync/atomic"
   "fmt"
+  "log"
+  "net/http"
+  "sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        cfg.fileserverHits.Add(1)
-        log.Printf("Hit! Count: %d", cfg.fileserverHits.Load())
-        next.ServeHTTP(w, r)
-    })
-}
-
 func main() {
   const filepathRoot = "."
   const port = "8080"
-  apiCfg := apiConfig{}
+
+  apiCfg := apiConfig{
+	fileserverHits: atomic.Int32{},
+  }
 
   mux := http.NewServeMux()
   mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	  w.WriteHeader(http.StatusOK)
-	  io.WriteString(w, "OK")
-  })
-  mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
-	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	  w.WriteHeader(http.StatusOK)
-	  apiCfg.fileserverHits.Store(0)
-  })
-  mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
-	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	  w.WriteHeader(http.StatusOK)
-	  io.WriteString(w, fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load()))
-  })
+  mux.HandleFunc("/healthz", handlerReadiness)
+  mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+  mux.HandleFunc("/reset", apiCfg.handlerReset)
 
   srv := &http.Server{
     Addr:       ":" + port,
@@ -50,6 +32,20 @@ func main() {
 
   log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
   log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
 }
 
 
