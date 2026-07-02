88442b80476b0da3bbd2bfa6c0a41439a49d0c84
ForgeRoot <dylananne.v.eck@gmail.com>
2026-07-02 17:08:46 +0200

    Added stateful handlers as middleware

--- main.go
+++ main.go
@@ -3,14 +3,45 @@ package main
 import (
   "net/http"
   "log"
+  "io"
+  "sync/atomic"
+  "fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        log.Printf("Hit! Count: %d", cfg.fileserverHits.Load())
+        next.ServeHTTP(w, r)
+    })
+}
 
 func main() {
   const filepathRoot = "."
   const port = "8080"
+  apiCfg := apiConfig{}
 
   mux := http.NewServeMux()
-  mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+  mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	  w.WriteHeader(http.StatusOK)
+	  io.WriteString(w, "OK")
+  })
+  mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	  w.WriteHeader(http.StatusOK)
+	  apiCfg.fileserverHits.Store(0)
+  })
+  mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+	  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	  w.WriteHeader(http.StatusOK)
+	  io.WriteString(w, fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load()))
+  })
 
   srv := &http.Server{
     Addr:       ":" + port,
